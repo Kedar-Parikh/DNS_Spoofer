231400e85ee1ea2143b44df0c9d1a13c96bf9861
Kedar-Parikh <kedarparikh2016@gmail.com>
2026-01-30 04:07:36 +0530

    Converted nfq packet to scapy packet

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Creating main script")
+import netfilterqueue as nfq
+import scapy.all as scapy
 
+def processpacket(packet):
+    spacket = scapy.IP(packet.get_payload())
+    print(spacket.show())
+    packet.accept()
+try:
+    queue = nfq.NetfilterQueue()
+    queue.bind(0, processpacket)
+    queue.run()
+except KeyboardInterrupt:
+    print("Quitting...")
+    exit(0)
